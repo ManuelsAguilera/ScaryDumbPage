dfcbd86cc9e671d031d72016a228ca7ba98556b9
manuelsaguilera <maguileraespejo@gmail.com>
2023-11-10 14:36:24 -0300

    adding pong game

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 
 
 // index.js
-
 function writeElem(elementId) {
     // Get the content of the specified element from elementos.html
-    var elementos = document.getElementById(elementId);
+    var element = document.getElementById(elementId);
     var div = document.getElementById("writeElems");
-    div.removeChild(div.firstElementChild)
-    div.appendChild(elementos)
+    var log = document.getElementById("log");
 
-    
+    // Log the current element to the hidden log div
+    log.innerText =  elementId;
+
+    console.log(log.innerHTML)
+    // Remove the current content of writeElems div
+
+    while (div.firstChild != null)
+    {
+        div.removeChild(div.firstChild)
+    }
+
+    // Append the new element to the writeElems div
+    div.append(element);
 }
+//pong
+
