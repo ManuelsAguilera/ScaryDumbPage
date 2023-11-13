b37f72175bc857ba6c84500d60a26f542e829e53
Your Name <you@example.com>
2023-11-13 15:48:25 -0300

    Añadido inicio del juego pong, y arreglado cosas feas

---
+++ pong.js
@@ -0,0 +1,13 @@
+//NECESITO UN CODIGO para hacer un juego de pong
+
+//make left paddle follow mouse
+
+document.addEventListener("DOMContentLoaded", function () {
+    const leftPaddle = document.getElementById("left");
+  
+    document.addEventListener("mousemove", function (event) {
+      const mouseY = event.clientY;
+      leftPaddle.style.top = mouseY - leftPaddle.offsetHeight / 2 + "px";
+    });
+  });
+  
