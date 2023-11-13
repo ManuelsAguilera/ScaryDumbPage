15e3eb9376f8f475c58468038ea8b57f35f28d6d
Your Name <you@example.com>
2023-11-13 16:25:52 -0300

    Añadido secret 1

--- pong.js
+++ pong.js
@@ -4,7 +4,10 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const leftPaddle = document.getElementById("left");
-  
+
+    //ball move
+    
+    //mouse move
     document.addEventListener("mousemove", function (event) {
       const mouseY = event.clientY;
       leftPaddle.style.top = mouseY - leftPaddle.offsetHeight / 2 + "px";
