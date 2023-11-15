fcebf32760388c0c29269169057efba55d5c40c4
Your Name <you@example.com>
2023-11-15 13:29:30 -0300

    fixed menu, added new tab.

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-
+const log = 0;
 
 // index.js
 function writeElem(elementId) {
     // Get the content of the specified element from elementos.html
     var element = document.getElementById(elementId);
     var div = document.getElementById("writeElems");
-    var log = document.getElementById("log");
+    
 
     // Log the current element to the hidden log div
     log.innerText =  elementId;
 
-    console.log(log.innerHTML)
-    // Remove the current content of writeElems div
-
-    while (div.firstChild != null)
-    {
-        div.removeChild(div.firstChild)
-    }
-
+    div.innerHTML = element.innerHTML;
+    copyElement = div.getElementById(elementId)
+    //copyElement.
     // Append the new element to the writeElems div
-    div.append(element);
+    
 }
 //pong
 
